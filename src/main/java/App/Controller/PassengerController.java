402407009360daf465b6a0b0fb488287f5c80cc1
Zheka Skipper <zheniachok@gmail.com>
2022-05-28 14:57:24 +0300

    spring

--- src/main/java/App/Controller/PassengerController.java
+++ src/main/java/App/Controller/PassengerController.java
@@ -1,7 +1,19 @@
 package App.Controller;
 
+import App.Entity.Passenger;
+import App.Repository.PassengerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class PassengerController {
+    private PassengerRepository passengerRepository;
+    @GetMapping("/hello/")
+    public Passenger hello() {
+        Passenger passenger = new Passenger();
+        passenger = passengerRepository.getById(1);
+        return passenger;
+    }
 }
